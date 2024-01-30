32fbde3683c0d482461c93c7169098b830ec5258
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-01-30 15:12:56 +0530

    ade

---
+++ dist/function.js
@@ -0,0 +1,14 @@
+"use strict";
+// function
+const getData = (product) => {
+    //   product.id = "sasas";
+    console.log(product);
+};
+const productOne = {
+    name: "Lenovo",
+    stock: 46,
+    price: 9999,
+    photo: "sample",
+    id: "aksjaklsjkj",
+};
+getData(productOne);
--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,1 @@
 "use strict";
-// function
-const getData = (product) => {
-    //   product.id = "sasas";
-    console.log(product);
-};
-const productOne = {
-    name: "Lenovo",
-    stock: 46,
-    price: 9999,
-    photo: "sample",
-    id: "aksjaklsjkj",
-};
-getData(productOne);
