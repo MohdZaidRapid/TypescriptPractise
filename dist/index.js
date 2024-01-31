3017b00a1b5598a4b8c7f9e9a6ad617b84c0b990
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-01-31 14:20:16 +0530

    added

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,10 @@
 // Type Assertion
 // const btn = document.getElementById("btn") as HTMLElement;
 // const btn = <HTMLElement>document.getElementById("btn");
-const btn = document.getElementById("btn");
-btn.onclick;
+// const btn = document.getElementById("btn")!;
+// btn.onclick;
+// // const img = document.getElementById("myimg") as HTMLImageElement;
+const myobj = {
+    name: "zaid",
+    email: "zaid@gamil.com",
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
 // Type Assertion
 // const btn = document.getElementById("btn") as HTMLElement;
 // const btn = <HTMLElement>document.getElementById("btn");
-const btn = document.getElementById("btn")!;
+// const btn = document.getElementById("btn")!;
+// btn.onclick;
+// // const img = document.getElementById("myimg") as HTMLImageElement;
 
-btn.onclick;
+// const img = document.querySelector("img")!;
+// // img?.src;
+
+// img.src;
+
+// const form = document.getElementById("myform") as HTMLFormElement;
+// const myinput = document.querySelector("form > input") as HTMLInputElement;
+
+// form.onsubmit = (e: SubmitEvent) => {
+//   e.preventDefault();
+//   const value = Number(myinput.value);
+//   console.log(value);
+//   console.log(typeof value);
+
+//   const h2 = document.createElement("h2");
+
+//   h2.textContent = String(value + 20);
+//   const body = document.querySelector("body")!;
+//   body.append(h2);
+// };
+interface Person {
+  name: string;
+  email: string;
+}
+
+const myobj: Person = {
+  name: "zaid",
+  email: "zaid@gamil.com",
+};
