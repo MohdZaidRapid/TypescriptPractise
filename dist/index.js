d36c0a2f9ed8515258bef0749b2fb4f47ecf47fc
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-01-31 18:56:37 +0530

    added

--- dist/index.js
+++ dist/index.js
@@ -11,14 +11,20 @@ const users = [
     },
     {
         name: "Random",
-        age: 52,
+        age: 21,
     },
     {
         name: "Random2",
         age: 59,
     },
 ];
+// const filterByPeoples = (arr: [], property: any, value: any) => {
+//   arr.filter((item) => item[property] === value);
+// };
 const filterByPeoples = (arr, property, value) => {
-    arr.filter((item) => item[property] === value);
+    return arr.filter((item) => item[property] === value);
 };
 const filteredPeopleByName = filterByPeoples(users, "name", "zaid");
+const filteredPeopleByAge = filterByPeoples(users, "age", 21);
+// age: 12, name: "zaid"
+console.log(filteredPeopleByName);
--- src/index.ts
+++ src/index.ts
@@ -85,7 +85,7 @@ const users: Person[] = [
   },
   {
     name: "Random",
-    age: 52,
+    age: 21,
   },
   {
     name: "Random2",
@@ -93,10 +93,20 @@ const users: Person[] = [
   },
 ];
 
-const filterByPeoples = (arr: [], property: any, value: any) => {
-  arr.filter((item) => item[property] === value);
+// const filterByPeoples = (arr: [], property: any, value: any) => {
+//   arr.filter((item) => item[property] === value);
+// };
+
+const filterByPeoples = <T, U extends keyof T>(
+  arr: T[],
+  property: U,
+  value: T[U]
+): T[] => {
+  return arr.filter((item) => item[property] === value);
 };
 
-
-
 const filteredPeopleByName = filterByPeoples(users, "name", "zaid");
+const filteredPeopleByAge = filterByPeoples(users, "age", 21);
+
+// age: 12, name: "zaid"
+console.log(filteredPeopleByName);
