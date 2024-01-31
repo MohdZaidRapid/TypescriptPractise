903717dbae21608809110fd7fd13ddeb6351c83d
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-01-31 14:32:24 +0530

    added

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,2 @@
 "use strict";
-// Type Assertion
-// const btn = document.getElementById("btn") as HTMLElement;
-// const btn = <HTMLElement>document.getElementById("btn");
-// const btn = document.getElementById("btn")!;
-// btn.onclick;
-// // const img = document.getElementById("myimg") as HTMLImageElement;
-const myobj = {
-    name: "zaid",
-    email: "zaid@gamil.com",
-};
+// utility classesz
---
+++ src/HtmlAsertion.ts
@@ -0,0 +1,60 @@
+// // Type Assertion
+// // const btn = document.getElementById("btn") as HTMLElement;
+// // const btn = <HTMLElement>document.getElementById("btn");
+// // const btn = document.getElementById("btn")!;
+// // btn.onclick;
+// // // const img = document.getElementById("myimg") as HTMLImageElement;
+
+// // const img = document.querySelector("img")!;
+// // // img?.src;
+
+// // img.src;
+
+// // const form = document.getElementById("myform") as HTMLFormElement;
+// // const myinput = document.querySelector("form > input") as HTMLInputElement;
+
+// // form.onsubmit = (e: SubmitEvent) => {
+// //   e.preventDefault();
+// //   const value = Number(myinput.value);
+// //   console.log(value);
+// //   console.log(typeof value);
+
+// //   const h2 = document.createElement("h2");
+
+// //   h2.textContent = String(value + 20);
+// //   const body = document.querySelector("body")!;
+// //   body.append(h2);
+// // };
+// // interface Person {
+// //   name: string;
+// //   email: string;
+// // }
+
+// interface Person {
+//   //   [key: string]: string;
+//   name: string;
+//   email: string;
+// }
+
+// const myobj: Person = {
+//   name: "zaid",
+//   email: "zaid@gamil.com",
+// };
+
+// const getName = (): string => {
+//   return myobj["name"];
+// };
+
+// const getEmail = (): string => {
+//   return myobj["email"];
+// };
+
+// const getData = (key: keyof Person): string => {
+//   return myobj[key];
+// };
+
+// // myobj[]
+// let key = "name";
+// myobj[key as keyof typeof myobj];
+
+// getData("name");
