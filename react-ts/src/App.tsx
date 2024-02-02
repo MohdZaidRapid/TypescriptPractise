ed40200ac6886d6060894f7bb45d04f80a9072ae
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-02 17:45:04 +0530

    added

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,5 +1,5 @@
-import { FormEvent, useState } from "react";
-import Box from "./components/Box";
+import { FormEvent, ReactNode, createContext, useState } from "react";
+// import Box from "./components/Box";
 
 // interface Person {
 //   name: string;
@@ -13,38 +13,62 @@ import Box from "./components/Box";
 //     e.preventDefault();
 //     console.log(user);
 //   };
-  // return (
-  //   <>
-  //     <div>
-  //       {/* <Box label="Search" value={val} setter={setVal} /> */}
-  //       <form onSubmit={submitHandler}>
-  //         <input
-  //           type="number"
-  //           placeholder="Age"
-  //           value={user?.age || ""}
-  //           onChange={(e) =>
-  //             setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
-  //           }
-  //         />
-  //         <input
-  //           type="text"
-  //           placeholder="Name"
-  //           value={user?.name || ""}
-  //           onChange={(e) =>
-  //             setUser((prev) => ({ ...prev!, name: e.target.value }))
-  //           }
-  //         />
-  //         <button type="submit">Register</button>
-  //       </form>
-  //     </div>
-  //   </>
-  // );
+// return (
+//   <>
+//     <div>
+//       {/* <Box label="Search" value={val} setter={setVal} /> */}
+//       <form onSubmit={submitHandler}>
+//         <input
+//           type="number"
+//           placeholder="Age"
+//           value={user?.age || ""}
+//           onChange={(e) =>
+//             setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
+//           }
+//         />
+//         <input
+//           type="text"
+//           placeholder="Name"
+//           value={user?.name || ""}
+//           onChange={(e) =>
+//             setUser((prev) => ({ ...prev!, name: e.target.value }))
+//           }
+//         />
+//         <button type="submit">Register</button>
+//       </form>
+//     </div>
+//   </>
+// );
 
-  // return()
+// return()
 // }
 
-function App(){
-  return <div></div>
+type ThemeType = "light" | "dark";
+
+interface ThemeContextType {
+  theme: ThemeType;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | null>({
+  theme: "light",
+  toggleTheme: () => {},
+});
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ThemeContext.Provider value={{ theme: "light", toggleTheme: () => {} }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+function App() {
+  return (
+    <ThemeProvider>
+      <div>Hello</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
