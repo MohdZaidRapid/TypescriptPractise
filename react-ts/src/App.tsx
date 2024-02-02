b9b3738d38d3ac37b586b2ed00028121d3bea444
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-02 17:26:03 +0530

    added

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,15 +1,50 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Box from "./components/Box";
 
-function App() {
-  const [] = useState();
-  return (
-    <>
-      <div>
-        <Box label="Search" value={""} onChange={() => {}} />
-      </div>
-    </>
-  );
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+
+// function App() {
+//   const [user, setUser] = useState<Person>();
+
+//   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     console.log(user);
+//   };
+  // return (
+  //   <>
+  //     <div>
+  //       {/* <Box label="Search" value={val} setter={setVal} /> */}
+  //       <form onSubmit={submitHandler}>
+  //         <input
+  //           type="number"
+  //           placeholder="Age"
+  //           value={user?.age || ""}
+  //           onChange={(e) =>
+  //             setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
+  //           }
+  //         />
+  //         <input
+  //           type="text"
+  //           placeholder="Name"
+  //           value={user?.name || ""}
+  //           onChange={(e) =>
+  //             setUser((prev) => ({ ...prev!, name: e.target.value }))
+  //           }
+  //         />
+  //         <button type="submit">Register</button>
+  //       </form>
+  //     </div>
+  //   </>
+  // );
+
+  // return()
+// }
+
+function App(){
+  return <div></div>
 }
 
 export default App;
--- react-ts/src/components/Box.tsx
+++ react-ts/src/components/Box.tsx
@@ -1,35 +1,40 @@
-import { ReactNode } from "react";
+import { Dispatch, SetStateAction } from "react";
 
 // type PropsType={
 //     heading:string;
 //     count?:number;
 //     children:ReactNode
-   
+
 // }
 
-type InputValType=string|number
+type InputValType = string | number;
 
-const Box=<T extends InputValType>({label,value,onChange}:{
-label:string,
-value:T,
-onChange:()=>void
-})=> {
-   
+const Box = <T extends InputValType>({
+  label,
+  value,
+  setter,
+}: {
+  label: string;
+  value: T;
+  setter: Dispatch<SetStateAction<T>>;
+}) => {
   return (
     <div>
-        {/* <h1>{heading}</h1>
+      {/* <h1>{heading}</h1>
         {count&&<h1>{count}</h1>}
         {children} */}
 
-        <form>
-            <label>
-              {label}
-            </label>
-            <input type="text" value={value} onChange={onChange} />
-            <button type="submit">Submit</button>
-        </form>
+      <form>
+        <label>{label}</label>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setter(e.target.value as T)}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Box
+export default Box;
